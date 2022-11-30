a9c7fc59e9ef71220f684e6643454c6342cfbccf
Megan Paquin <106716810+meganpaquin@users.noreply.github.com>
2022-11-30 15:22:54 -0500

    first commit of fixed project

---
+++ accounts/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import SignUpForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = SignUpForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ["email", "username",]
+
+admin.site.register(CustomUser, CustomUserAdmin)
